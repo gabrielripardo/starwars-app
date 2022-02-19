3f2ec6e564e05941209b942d209f7740c6a7cac0
Gabriel <gts.senna@gmail.com>
2022-02-19 18:51:40 -0300

    fix: configurações para eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import Home from './pages'
+import Home from './pages/Home'
 
 function App() {
-  return (
-    <>
-      <h1>App component teste</h1>
-      <Home />
-    </>
-  )
+    return (
+        <>
+            <h1>App component teste</h1>
+            <Home />
+        </>
+    )
 }
 
 export default App
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import Table from 'components/Table'
+
+function Home() {
+    return (
+        <>
+            <h1>Página Home</h1>
+            <Table />
+        </>
+    )
+}
+
+export default Home
