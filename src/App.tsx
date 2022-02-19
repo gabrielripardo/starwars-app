96322c302eb9c1666e8586f79517fafb18a9e47e
Gabriel <gts.senna@gmail.com>
2022-02-19 17:50:23 -0300

    configuração de ambiente de desenvolvimento

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import Home from './pages'
+
 function App() {
   return (
     <>
-      <h1>App component</h1>
+      <h1>App component teste</h1>
+      <Home />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
